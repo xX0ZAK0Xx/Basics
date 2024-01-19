3c9448ff49723bacb1e95202151075bfce6b37ac
Md. Zayed Oyshik <zayedoyshik1@gmail.com>
2024-01-19 15:24:22 +0600

    This is my First Project

---
+++ lib/main.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  // emn ekta function, jeta app ke run koraabe
+  runApp(MyApp(name: "Rabby",));
+}
+
+class MyApp extends StatelessWidget {
+
+  const MyApp({super.key, required this.name});
+  final String name;
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          backgroundColor: const Color.fromARGB(255, 255, 64, 0),
+          actions: const [
+            Icon(Icons.search, size: 30, color:  Colors.white,),
+          ],
+          
+          title: 
+            const Text(
+              "This is first screen", 
+              style: TextStyle(
+                color: Colors.white, 
+                fontSize: 30,
+                fontWeight: FontWeight.bold
+              ),
+          ),
+          leading: const Icon(Icons.menu, color:  Colors.white,),
+        ),
+        backgroundColor: Colors.green,
+        body: Center(
+          child: Text(name, style: TextStyle(color: Colors.white, fontSize: 50),
+          ),
+        ),
+      ),
+    );
+  }
+}
